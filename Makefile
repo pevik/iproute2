pevik/iproute2

commit 8804a8c0d387c12155c5844fcc8290e1712dfbe9

Makefile: Add check target
Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

Makefile
@@ -77,6 +77,7 @@ help:
 	@echo " clean               - remove products of build"
 	@echo " distclean           - remove configuration and build"
 	@echo " install             - install binaries on local machine"
+	@echo " check               - run tests"
 	@echo " cscope              - build cscope database"
 	@echo " snapshot            - generate version number header"
 	@echo ""
@@ -117,6 +118,9 @@ clobber:
 	rm -f config.mk cscope.*
 
 distclean: clobber
+
+check:
+	cd testsuite && $(MAKE) && $(MAKE) alltests
 
 cscope:
 	cscope -b -q -R -Iinclude -sip -slib -smisc -snetem -stc

commit aa7596558bbb4fabc3a3d412f73b6169dcbfa15a

Makefile: have check target depend on all
Otherwise it will simply fail immediately from a just-cleaned
workspace:

$ make check -j1
cd testsuite && make && make alltests
echo "Entering iproute2" && cd iproute2 && make configure && cd ..;
Entering iproute2
make -C tools
Makefile:3: ../../config.mk: No such file or directory
make[2]: *** No rule to make target '../../config.mk'.  Stop.

Fixes: 8804a8c0d387 ("Makefile: Add check target")
Cc: petr.vorel@gmail.com

Signed-off-by: Luca Boccassi <bluca@debian.org>

Makefile
@@ -119,7 +119,7 @@ clobber:
 
 distclean: clobber
 
-check:
+check: all
 	cd testsuite && $(MAKE) && $(MAKE) alltests
 
 cscope: